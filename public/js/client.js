4d39248c74eddf4949a42f20fb4a769c00091abe
nochmehrmango <nochmehrmango@gmail.com>
2025-12-01 10:44:51 +0100

    Steine sammeln
    Button + Funktionalitäten für Steine sammeln hinzugefügt

--- public/js/client.js
+++ public/js/client.js
@@ -1,6 +1,9 @@
 const btnHacken = document.getElementById('btn-holz-hacken');
 const holzAnzeige = document.getElementById('holz-anzeige');
 
+const btnSteinBrechen = document.getElementById('btn-stein-brechen');
+const steinAnzeige = document.getElementById('stein-anzeige');
+
 btnHacken.addEventListener('click', () => {
     btnHacken.disabled = true;
     fetch('/api/hacken', { method: 'POST' })
@@ -13,4 +16,20 @@ btnHacken.addEventListener('click', () => {
         }
     })
     .finally(() => btnHacken.disabled = false);
+});
+
+
+//neu
+btnSteinBrechen.addEventListener('click', () => {
+    btnSteinBrechen.disabled = true;
+    fetch('/api/steinbrechen', {method: 'POST'})
+    .then(r => r.json())
+    .then(data =>{
+        if(data.success){
+            steinAnzeige.innerText = data.neueWerte.stein;
+            steinAnzeige.style.color = '#ecf0f1';
+            setTimeout(() => steinAnzeige.style.color = '', 300);
+        }
+    })
+    .finally(()=> btnSteinBrechen.disabled = false);
 });
